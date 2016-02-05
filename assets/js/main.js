77b81c596c2e51c75e9ff0f83ca472a7050be901
ein3108 <ein3108@gmail.com>
2016-02-05 21:41:58 +0000

    Revert to 3cb1972e0348ad80d349a06d1d4f40c82d952b3a

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,10 +39,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'assets/images/bg01.jpg': 'center',
-							'assets/images/bg02.jpg': 'center',
-							'assets/images/bg03.jpg': 'center',
-              'assets/images/bg04.jpg': 'center'
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
