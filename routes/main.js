e94c4385c76819d1d66664b9c7b2f8931e278279
ein3108 <ein3108@gmail.com>
2016-02-07 23:16:53 +0000

    main.html is served both when / or /main is approached

--- routes/main.js
+++ routes/main.js
@@ -4,10 +4,12 @@ var router = express.Router();
 var app = express();
 var http = require('http').Server(app);
 
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
   res.render('main', { title: 'Chefbox', version: '0.0.1' });
 });
 
-http.listen(4200);
+router.get('/main', function(req, res) {
+  res.render('main', { title: 'Chefbox', version: '0.0.1' });
+});
 
 module.exports = router;
