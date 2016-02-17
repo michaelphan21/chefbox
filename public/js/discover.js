cb2b0d3540d091716ee183b1435a251602464c5e
Hyeonsu Kang <ein3108@gmail>
2016-02-17 04:16:25 +0000

    Modified the discover view and connected it with the view chain using jQuery

---
+++ public/js/discover.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(document).ready(function() {
+	console.log("$(document).read()");
+	initializePage();
+});
+
+function initializePage() {
+	console.log("initializePage()");
+
+	$('button').click(function() {
+		var foodID = $(this).closest('.wrapper').attr('id');
+		console.log("foodID:" + foodID);
+		var foodInfoURL = "/searchID?id=" + foodID;
+		$.get(foodInfoURL, function(data) {
+			document.location.href = "foodinfo?idx=" + data;
+		});
+	});
+}
