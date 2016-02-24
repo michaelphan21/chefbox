1759c6a846ed6fc90075a8871f6eb38b607d4010
Hyeonsu Kang <ein3108@gmail.com>
2016-02-24 00:03:18 +0000

    (Not fully functional) Order view implemented

--- public/js/search.js
+++ public/js/search.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
 
 function initializePage() {
 	console.log("initializePage()");
-	$('#moreBtn').click(function() {
+	$('#moreBtn').click(function(e) {
+		e.preventDefault();
 		console.log("moreBtn clicked");
 	});
-	$('.col-md-3.portfolio-item button:nth-child(1)').click(function() {
-		console.log("loadFoodItem()");
-		console.log(".col-md-3.portfolio-item button length:" + $('.col-md-3.portfolio-item button').children().length);
 
+	$('.btn-default-order').click(function() {
+		console.log(".btn-default-order clicked");
 		var foodID = $(this).closest('.col-md-3.portfolio-item').attr('id');
 		console.log("foodID:" + foodID);
 		var foodInfoURL = "/searchID?id=" + foodID;
@@ -21,42 +21,39 @@ function initializePage() {
 			document.location.href = "foodinfo?idx=" + data;
 		});
 	});
-	$('.col-md-3 .btn-default').click(function() {
-		console.log('default button clicked');
-		var foodID = $(this).closest('.col-md-3.portfolio-item').attr('id');
-		var foodInfoURL = "/searchID?id=" + foodID;
-		$.get(foodInfoURL, function(data) {
-			document.location.href = "foodinfo?idx=" + data;
-		});
-	});
-	$('.col-md-3 .btn-primary').click(function() {
-		console.log('primary button clicked');
-		var foodID = $(this).closest('.col-md-3.portfolio-item').prop('price');
-		console.log(foodID);
-		var foodInfoURL = "/searchID?id=" + foodID;
-		$.get(foodInfoURL, function(data){
-			console.log(foodInfoURL);
-			//var newcount = data['count']--;
-			//data['count'] = newcount;
-		});
 
-		/*
-		var divcontainer = $('#container');
-		var keywordsearch = $('#keyword-search');
-		keywordsearch.css("backgroundColor", "rgba(0,0,0,0.01)");
-		keywordsearch.css("border-color", "rgba(0,0,0,0.01)")
-		keywordsearch.attr('disabled', 'disabled');
-		divcontainer = divcontainer.prepend('<div id="orderviewbground" style="position:absolute; top: -20px; left: 0; right: 0; bottom: 0; z-index: 2; background-color:rgba(0, 0, 0, 0.4);"></div>');
-		var foodID = $(this).closest('.col-md-3.portfolio-item').attr('id');
-		var foodInfoURL = "/searchData?id=" + foodID;
-		$.get(foodInfoURL, function(data) {
-			//divcontainer = divcontainer.appendTo('<div id="orderview" style="position: absolute; top: -20px; left: 0; right: 0; bottom: 0; z-index: 100;"></div>');
-			var html = '<div class="container" style="padding: 0px; margin: 0px; background-color: #000; color: #fff"><p>' + data.price + '</p>';
-			html += '<table><thead><tr><th>Need by</th><th>&nbsp;&nbsp;&nbsp;&nbsp;</th><th>How much portion?</th></tr></thead><tbody><tr><td><input id="keyword-search" type="text" class="form-control"  placeholder="mm/dd/yyyy" style="text-align: center;"/></td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input id="keyword-search" type="text" class="form-control"  placeholder="1 person&apos;s" style="text-align: center;"/></td></tr></tbody></table>';
-			html += '<br><button type="button" class="btn btn-primary">Request to Order</button></div>';
-			html += '<p></p>';
-			divcontainer = $(html).prependTo(divcontainer);
-		});
-		*/
-	});	
+	$('.btn-primary-order').click(function() {
+		console.log('.btn-primary-order clicked');
+	});
+
+	$('.btn-default-select').click(function() {
+		console.log('.btn-default-select clicked');
+	});
+
+	$('#checkout-btn').click(function() {
+		console.log('#checkout-btn clicked');
+	});
+	
+	$('#message-btn').click(function() {
+		console.log('#message-btn clicked');
+	});
+	
+	/*
+	var divcontainer = $('#container');
+	var keywordsearch = $('#keyword-search');
+	keywordsearch.css("backgroundColor", "rgba(0,0,0,0.01)");
+	keywordsearch.css("border-color", "rgba(0,0,0,0.01)")
+	keywordsearch.attr('disabled', 'disabled');
+	divcontainer = divcontainer.prepend('<div id="orderviewbground" style="position:absolute; top: -20px; left: 0; right: 0; bottom: 0; z-index: 2; background-color:rgba(0, 0, 0, 0.4);"></div>');
+	var foodID = $(this).closest('.col-md-3.portfolio-item').attr('id');
+	var foodInfoURL = "/searchData?id=" + foodID;
+	$.get(foodInfoURL, function(data) {
+		//divcontainer = divcontainer.appendTo('<div id="orderview" style="position: absolute; top: -20px; left: 0; right: 0; bottom: 0; z-index: 100;"></div>');
+		var html = '<div class="container" style="padding: 0px; margin: 0px; background-color: #000; color: #fff"><p>' + data.price + '</p>';
+		html += '<table><thead><tr><th>Need by</th><th>&nbsp;&nbsp;&nbsp;&nbsp;</th><th>How much portion?</th></tr></thead><tbody><tr><td><input id="keyword-search" type="text" class="form-control"  placeholder="mm/dd/yyyy" style="text-align: center;"/></td><td>&nbsp;&nbsp;&nbsp;&nbsp;</td><td><input id="keyword-search" type="text" class="form-control"  placeholder="1 person&apos;s" style="text-align: center;"/></td></tr></tbody></table>';
+		html += '<br><button type="button" class="btn btn-primary">Request to Order</button></div>';
+		html += '<p></p>';
+		divcontainer = $(html).prependTo(divcontainer);
+	});
+	*/
 }
